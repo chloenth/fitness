3dc9b361feb008032f94c7c52a2a9c7938f23b64
Huong <huongnguyen.nth1709@gmail.com>
2024-04-17 20:31:41 +0300

    add custom useMediaQuery to enable navbar change to hamburger button on mobile size

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import PageLink from "./PageLink";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -9,7 +11,10 @@ type Props = {
 };
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
   return (
     <nav>
@@ -20,37 +25,46 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <img src={Logo} alt="logo" />
 
             {/* Right Side */}
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} w-full gap-8 text-sm`}>
-                <PageLink
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} w-full gap-8 text-sm`}>
+                  <PageLink
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
 
-                <PageLink
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+                  <PageLink
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
 
-                <PageLink
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+                  <PageLink
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
 
-                <PageLink
-                  page="Contact us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+                  <PageLink
+                    page="Contact us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} w-full gap-8`}>
+                  <p>Sign In</p>
+                  <button>Become a Member</button>
+                </div>
               </div>
-              <div className={`${flexBetween} w-full gap-8`}>
-                <p>Sign In</p>
-                <button>Become a Member</button>
-              </div>
-            </div>
+            ) : (
+              <button
+                className="bg-secondary-500 rounded-full p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,9 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {};
+
+const ActionButton = (props: Props) => {
+  return <AnchorLink>ActionButton</AnchorLink>;
+};
+
+export default ActionButton;
