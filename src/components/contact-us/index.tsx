396f5022bd62bf833da5511b4c7674cca1772604
Huong <huongnguyen.nth1709@gmail.com>
2024-04-18 22:32:35 +0300

    update contact page with image

--- src/components/contact-us/index.tsx
+++ src/components/contact-us/index.tsx
@@ -153,6 +153,26 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               </div>
             </form>
           </motion.div>
+
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="md:before:content-evolvetext w-full before:absolute before:-bottom-24 before:-right-10 before:z-[-1]">
+              <img
+                src={ContactUsPageGraphic}
+                alt="contact-us-page-graphic"
+                className="w-full"
+              />
+            </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>
